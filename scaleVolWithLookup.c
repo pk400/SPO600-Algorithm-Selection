9c1468bd26a81805294a22fa5692bdd50abfa722
unknown <jaro@SY-T2113-W811.SeneDS.senecacollege.ca>
2015-10-07 12:39:20 -0400

    Added Debug logic

--- scaleVolWithLookup.c
+++ scaleVolWithLookup.c
@@ -26,7 +26,7 @@ int16_t scaleVolume(int16_t sample, float vol)
 
 int main(int argc, char** argv)
 {
-	if(argc < 3) { printf("Needs 2 parameters... Try again."); return 0; }
+	if(argc < 2) { printf("Needs 2 parameters... Try again."); return 0; }
 
 	// Setup
 	FILE *fin, *fout;
@@ -35,40 +35,40 @@ int main(int argc, char** argv)
 	int counter = 0, k, volchange[16], v = 0;
 	
 	fin = fopen(argv[1], "r");
+	#if DEBUG == 1
 	fout = fopen(argv[2], "w");
+	#endif
 	factor = 1.0;
 	
 	// Will lower factor every 1/16th step
 	for(k = 0; k < 16; volchange[k] = (NUM_ENTRIES/16) * k, k++);
 	
-	if(fin != NULL) {
-		if(fout != NULL) {
-			start = clock();
-			
-			// Process
-			while(fgets(line, 32, fin) != NULL) {
-				if(counter == volchange[v]) { // Reinitializes the array every 1/16th step
-					if(counter > 0) factor -= 0.0625;
-					initializeLookup();
-					v++;
-				}
-				int16_t scaledvolume = scaleVolume(atoi(line), factor);
-				counter++;
-				
-				#if DEBUG == 1
-				sprintf(res, "%.3f\t\t%d\n", factor, scaleVolume(atoi(line), factor));
-				fputs(res, fout);
-				#endif
-			}
-			
-			end = clock();
+	if(fin != NULL) {		
+		start = clock();
 		
-			printf("Program took %0.fms.", (double)end-start/CLOCKS_PER_SEC);
+		// Process
+		while(fgets(line, 32, fin) != NULL) {
+			if(counter == volchange[v]) { // Reinitializes the array every 1/16th step
+				if(counter > 0) factor -= 0.0625;
+				initializeLookup();
+				v++;
+			}
+			int16_t scaledvolume = scaleVolume(atoi(line), factor);
+			counter++;
 			
-			// Clean
-			fclose(fin);
-			fclose(fout);
-		} else { printf("Check destination file parameter. Try again."); }	
+			#if DEBUG == 1
+			sprintf(res, "%.3f\t\t%d\n", factor, scaleVolume(atoi(line), factor));
+			fputs(res, fout);
+			#endif
+		}
+		
+		end = clock();
+	
+		printf("Program took %0.fms.", (double)end-start/CLOCKS_PER_SEC);
+		
+		// Clean
+		fclose(fin);
+		fclose(fout);
 	} else { printf("Check source file parameter. Try again."); }
 	
 	return 0;
