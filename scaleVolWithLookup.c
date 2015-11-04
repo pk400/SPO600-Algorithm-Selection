ef42674fe6d74f24685f9ef2f78c5ed829a0701c
Joel Aro <joelaro@outlook.com>
2015-11-04 17:25:29 -0500

    Fixed missing include headers

--- scaleVolWithLookup.c
+++ scaleVolWithLookup.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <time.h>
 
 #define NUM_ENTRIES 1550000
@@ -66,7 +67,7 @@ int main(int argc, char** argv)
 		
 		end = clock();
 	
-		printf("Program took %0.fms.", (double)end-start/CLOCKS_PER_SEC);
+		printf("Program took %fms.", (double)end-start/CLOCKS_PER_SEC);
 		
 		fclose(fout);
 		fclose(fin);
